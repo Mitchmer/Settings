8cdc1f2e030558de7d61dcf425b31bd6a71836d5
Mitch Merrell <mitchmerrell@Mitchs-MacBook-Pro.local>
2019-08-05 11:11:41 -0600

    sets up the tableview cell with update views function and outlets

--- Settings/Settings/View Controllers/SettingTableViewCell.swift
+++ Settings/Settings/View Controllers/SettingTableViewCell.swift
@@ -12,10 +12,33 @@ class SettingTableViewCell: UITableViewCell {
 
     // MARK: Outlets
     
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var settingNameLabel: UILabel!
+    @IBOutlet weak var settingSwitch: UISwitch!
+    
     // MARK: Properties
     
-    var setting: Setting?
+    var setting: Setting? {
+        didSet {
+            updateViews()
+        }
+    }
     
     // MARK: Custom Methods
+    
+    func updateViews() {
+        
+        if let setting = setting {
+            
+            iconImageView.image = setting.icon
+            settingNameLabel.text = setting.name
+            settingSwitch.isOn = setting.isOn
+            
+            backgroundColor = setting.isOn ? .lightGray : .white
+            
+        } else {
+            
+        }
+    }
 
 }
